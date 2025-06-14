f72bc4bcfbad25e8790128461f286a5a7c862bfa
Jasper Yu <007seadog@gmail.com>
2025-06-14 19:40:17 +0800

    Change vite config so incidents.json in same on both prod and dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/data/incidents.json': {
+        target: 'http://localhost:5173',
+        rewrite: (path) => path.replace(/^\/data/, '/src/data')
+      }
+    }
+  }
 })
