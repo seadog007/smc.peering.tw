e6286228791c5d309086b3748e9074c116dab6d2
gnehs <gnehs@gnehs.net>
2025-10-22 05:47:49 +0800

    refactor: move map related components into folder

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,12 +1,12 @@
-import * as maplibregl from 'maplibre-gl';
-import { useEffect, useRef, useState } from 'react';
+import * as maplibregl from "maplibre-gl";
+import { useEffect, useRef, useState } from "react";
 
-import 'maplibre-gl/dist/maplibre-gl.css';
-import landingPoints from '../data/landing-points.json';
-import CableLayer from './CableLayer';
+import "maplibre-gl/dist/maplibre-gl.css";
+import landingPoints from "../../data/landing-points.json";
+import CableLayer from "./CableLayer";
 
-import './Map.css';
-import type { Feature, Geometry } from 'geojson';
+import "./Map.css";
+import type { Feature, Geometry } from "geojson";
 
 interface LandingPoint {
   name: string;
@@ -14,10 +14,10 @@ interface LandingPoint {
 }
 
 interface MapProps {
-  cableFilter?: 'all' | 'normal' | 'broken';
+  cableFilter?: "all" | "normal" | "broken";
 }
 
-export default function Map({ cableFilter = 'all' }: MapProps) {
+export default function Map({ cableFilter = "all" }: MapProps) {
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<maplibregl.Map | null>(null);
   const cableLayerRef = useRef<HTMLDivElement>(null);
@@ -35,55 +35,55 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
         version: 8,
         sources: {
           map: {
-            type: 'vector',
-            url: 'https://lb.exptech.dev/api/v1/map/tiles/tiles.json',
+            type: "vector",
+            url: "https://lb.exptech.dev/api/v1/map/tiles/tiles.json",
           },
         },
         layers: [
           {
-            id: 'background',
-            type: 'background',
+            id: "background",
+            type: "background",
             paint: {
-              'background-color': '#1f2025',
+              "background-color": "#0b0c0d",
             },
           },
           {
-            'id': 'county',
-            'type': 'fill',
-            'source': 'map',
-            'source-layer': 'city',
-            'paint': {
-              'fill-color': '#3F4045',
-              'fill-opacity': 1,
+            id: "county",
+            type: "fill",
+            source: "map",
+            "source-layer": "city",
+            paint: {
+              "fill-color": "#304046",
+              "fill-opacity": 1,
             },
           },
           {
-            'id': 'town',
-            'type': 'fill',
-            'source': 'map',
-            'source-layer': 'town',
-            'paint': {
-              'fill-color': '#3F4045',
-              'fill-opacity': 1,
+            id: "town",
+            type: "fill",
+            source: "map",
+            "source-layer": "town",
+            paint: {
+              "fill-color": "#304046",
+              "fill-opacity": 1,
             },
           },
           {
-            'id': 'county-outline',
-            'source': 'map',
-            'source-layer': 'city',
-            'type': 'line',
-            'paint': {
-              'line-color': '#a9b4bc',
+            id: "county-outline",
+            source: "map",
+            "source-layer": "city",
+            type: "line",
+            paint: {
+              "line-color": "#738B93",
             },
           },
           {
-            'id': 'global',
-            'type': 'fill',
-            'source': 'map',
-            'source-layer': 'global',
-            'paint': {
-              'fill-color': '#3F4045',
-              'fill-opacity': 1,
+            id: "global",
+            type: "fill",
+            source: "map",
+            "source-layer": "global",
+            paint: {
+              "fill-color": "#304046",
+              "fill-opacity": 1,
             },
           },
         ],
@@ -100,7 +100,7 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
     // Add navigation controls
     // map.current.addControl(new maplibregl.NavigationControl({ showCompass: false }), 'top-right');
 
-    map.current.on('load', () => {
+    map.current.on("load", () => {
       setMapLoaded(true);
     });
   }, []);
@@ -108,10 +108,21 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
   useEffect(() => {
     if (map.current) {
       if (isMobile) {
-        map.current.fitBounds([[118, 21.2], [124, 25.8]], { padding: 20, duration: 0 });
-      }
-      else {
-        map.current.fitBounds([[117, 20], [130, 28]], { padding: 20, duration: 0 });
+        map.current.fitBounds(
+          [
+            [118, 21.2],
+            [124, 25.8],
+          ],
+          { padding: 20, duration: 0 },
+        );
+      } else {
+        map.current.fitBounds(
+          [
+            [117, 20],
+            [130, 28],
+          ],
+          { padding: 20, duration: 0 },
+        );
       }
     }
   }, [isMobile]);
@@ -122,61 +133,61 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
     const currentMap = map.current;
     const landingMarkerList: Feature<Geometry>[] = [];
 
-    map.current.on('load', () => {
+    map.current.on("load", () => {
       const typedLandingPoints = landingPoints as unknown as LandingPoint[];
 
       for (const point of typedLandingPoints) {
         landingMarkerList.push({
-          type: 'Feature',
+          type: "Feature",
           properties: {
             name: point.name,
             coordinates: point.coordinates,
           },
           geometry: {
-            type: 'Point',
+            type: "Point",
             coordinates: point.coordinates,
           },
         });
       }
 
-      const sourceId = 'landing-marker-points';
+      const sourceId = "landing-marker-points";
       const source = currentMap.getSource(sourceId);
 
       if (!source) {
         currentMap.addSource(sourceId, {
-          type: 'geojson',
+          type: "geojson",
           data: {
-            type: 'FeatureCollection',
+            type: "FeatureCollection",
             features: landingMarkerList,
           },
         });
 
         // point shadow layer
         currentMap.addLayer({
-          id: 'landing-marker-shadow-layer',
-          type: 'circle',
+          id: "landing-marker-shadow-layer",
+          type: "circle",
           source: sourceId,
           paint: {
-            'circle-color': '#ffffff',
-            'circle-radius': 12,
-            'circle-blur': 0.8,
-            'circle-opacity': 0.4,
+            "circle-color": "#ffffff",
+            "circle-radius": 12,
+            "circle-blur": 0.8,
+            "circle-opacity": 0.4,
           },
         });
 
         // point normal layer
         currentMap.addLayer({
-          id: 'landing-marker-layer',
-          type: 'circle',
+          id: "landing-marker-layer",
+          type: "circle",
           source: sourceId,
           paint: {
-            'circle-radius': 7,
-            'circle-color': '#ffffff',
+            "circle-radius": 7,
+            "circle-color": "#ffffff",
           },
         });
 
         // bind click event
-        currentMap.on('click', 'landing-marker-layer', (event) => {
+        currentMap.on("click", "landing-marker-layer", (event) => {
           // Close existing popup if any
           if (popupRef.current) {
             popupRef.current.remove();
@@ -184,17 +195,20 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
 
           const properties = event.features![0].properties;
           // 被 maplibre 轉換成 string, 需自行轉回 array
-          const coordinates = JSON.parse(properties.coordinates as string) as number[];
+          const coordinates = JSON.parse(
+            properties.coordinates as string,
+          ) as number[];
 
           popupRef.current = new maplibregl.Popup({
             offset: 25,
             closeButton: true,
             closeOnClick: false,
-            className: 'landing-point-popup',
-            maxWidth: '250px',
+            className: "landing-point-popup",
+            maxWidth: "250px",
           })
             .setLngLat([coordinates[0], coordinates[1]])
-            .setHTML(`
+            .setHTML(
+              `
             <div style="padding: 10px 12px;">
               <h3 style="margin: 0 0 8px 0; padding-right: 20px; color: #48A9FF; font-size: 14px; font-weight: bold; word-wrap: break-word;">
                 ${properties.name}
@@ -205,26 +219,33 @@ export default function Map({ cableFilter = 'all' }: MapProps) {
                 </p>
               </div>
             </div>
-          `)
+          `,
+            )
             .addTo(currentMap);
         });
 
         // cursor pointer
-        currentMap.on('mouseenter', 'landing-marker-layer', () => {
-          currentMap.getCanvas().style.cursor = 'pointer';
+        currentMap.on("mouseenter", "landing-marker-layer", () => {
+          currentMap.getCanvas().style.cursor = "pointer";
         });
 
-        currentMap.on('mouseleave', 'landing-marker-layer', () => {
-          currentMap.getCanvas().style.cursor = '';
+        currentMap.on("mouseleave", "landing-marker-layer", () => {
+          currentMap.getCanvas().style.cursor = "";
         });
       }
     });
   }, []);
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <div ref={mapContainer} style={{ width: '100%', height: '100%' }} />
-      {mapLoaded && map.current && <CableLayer ref={cableLayerRef} map={map.current} cableFilter={cableFilter} />}
+    <div style={{ width: "100%", height: "100%" }}>
+      <div ref={mapContainer} style={{ width: "100%", height: "100%" }} />
+      {mapLoaded && map.current && (
+        <CableLayer
+          ref={cableLayerRef}
+          map={map.current}
+          cableFilter={cableFilter}
+        />
+      )}
     </div>
   );
 }
