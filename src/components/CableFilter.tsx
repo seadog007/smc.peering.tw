272943bdcd4a04304d1b3eedaeb3588d3c552335
gnehs <gnehs@gnehs.net>
2025-10-23 00:19:19 +0800

    Enhance CableFilter component with motion animations and improved visual effects

--- src/components/CableFilter.tsx
+++ src/components/CableFilter.tsx
@@ -24,22 +24,57 @@ export default function CableFilter({
   t,
 }: CableFilterProps) {
   return (
-    <div className="flex flex-col rounded-full border border-white/10 bg-[#19191B]/50 p-1 shadow-lg backdrop-blur-md select-none">
+    <div className="relative flex flex-col rounded-full bg-black/10 p-1 shadow-lg backdrop-blur-md select-none">
+      <div
+        className="pointer-events-none absolute inset-0 rounded-full bg-white/10"
+        style={{
+          maskImage:
+            "radial-gradient(circle at top, black 0%, transparent 60%)",
+        }}
+      />
+      <div
+        className="pointer-events-none absolute inset-0 rounded-full border-[0.5px] border-white/20"
+        style={{
+          maskImage:
+            "radial-gradient(circle at top, black 0%, rgba(0,0,0,.5) 60%)",
+        }}
+      />
       {filterOptions.map(({ icon: Icon, value, tooltipKey }) => (
         <Tooltip key={value} useTouch>
           <TooltipTrigger asChild>
-            <button
+            <motion.button
               className="relative flex size-10 items-center justify-center rounded-full"
               onClick={() => setCableFilter(value)}
+              whileHover={{
+                scale: 1.05,
+              }}
+              whileTap={{
+                scale: 0.95,
+              }}
             >
               <Icon className="size-6" />
               {cableFilter === value && (
                 <motion.div
-                  className="absolute inset-0 rounded-full bg-white/10"
+                  className="absolute inset-0 rounded-full bg-white/10 shadow-sm shadow-black/5"
                   layoutId="cable-filter-active-indicator"
-                />
+                >
+                  <div
+                    className="pointer-events-none absolute inset-0 rounded-full bg-white/10"
+                    style={{
+                      maskImage:
+                        "radial-gradient(circle at top, black 0%, transparent 60%)",
+                    }}
+                  />
+                  <div
+                    className="pointer-events-none absolute inset-0 rounded-full border-[0.5px] border-white/20"
+                    style={{
+                      maskImage:
+                        "radial-gradient(circle at top, black 0%, transparent 80%)",
+                    }}
+                  />
+                </motion.div>
               )}
-            </button>
+            </motion.button>
           </TooltipTrigger>
           <TooltipContent side="right">
             <p>{t(tooltipKey)}</p>
